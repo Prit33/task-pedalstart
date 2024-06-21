418939bcd960e128d07929edb43f2f7d16057c50
Prit33 <prityadav1310@gmail.com>
2024-06-21 12:49:00 +0530

    removed url file (uploaded by mistake!)

--- client/src/url.js
+++
@@ -1,3 +0,0 @@
-// export const URL=import.meta.env.VITE_URL
-export const URL="https://devforum-backend-testing.onrender.com"
-export const IF="https://devforum-backend-testing.onrender.com/images/"
